0b1214ba570518a7005c0d70943a03cb489b8e5b
ivan <sinitsinvanya@gmail.com>
2019-03-01 14:33:39 +0400

    commented spreadsheet service class.

--- main.py
+++ main.py
@@ -8,119 +8,6 @@ from utils.google_services import (
     initialize_google_drive_service,
     initialize_google_spreadsheet_service,
 )
-from utils.spreadsheet_handler import SpreadSheetHandler
-from utils.drive_handler import DriveHandler
-from social import social_platform_object_factory
-
-
-def process_spreadsheet(spreadsheet_id):
-    google_scope = [
-        'https://www.googleapis.com/auth/spreadsheets',
-        'https://www.googleapis.com/auth/drive',
-    ]
-
-    service_account_credentials = os.getenv('GOOGLE_SERVICE_ACCOUNT_CREDENTIALS')
-
-    spreadsheet_service = initialize_google_spreadsheet_service(
-        google_scope, service_account_credentials
-    )
-    drive_service = initialize_google_drive_service(
-        google_scope, service_account_credentials
-    )
-
-    spreadsheet_handler = SpreadSheetHandler(
-        spreadsheet_service, spreadsheet_id=spreadsheet_id
-    )
-
-    drive_handler = DriveHandler(drive_service)
-
-    social_platform_authorization_config = {
-        'vk_login': os.getenv('VK_LOGIN'),
-        'vk_app_id': os.getenv('VK_APP_ID'),
-        'vk_token': os.getenv('VK_TOKEN'),
-        'telegram_token': os.getenv('TELEGRAM_TOKEN'),
-        'facebook_api_token': os.getenv('FACEBOOK_TOKEN'),
-    }
-
-    social_platform_post_config = {
-        'telegram_chat_id': os.getenv('TELEGRAM_CHAT_ID'),
-        'vk_group_id': os.getenv('VK_GROUP_ID'),
-        'vk_album_id': os.getenv('VK_ALBUM_ID'),
-        'facebook_group_id': os.getenv('FACEBOOK_GROUP_ID'),
-    }
-
-    telegram_social = social_platform_object_factory.create(
-        'telegram', **social_platform_authorization_config
-    )
-    vk_social = social_platform_object_factory.create(
-        'vk', **social_platform_authorization_config
-    )
-    facebook_social = social_platform_object_factory.create(
-        'facebook', **social_platform_authorization_config
-    )
-
-    spreadsheet_handler.parse_spreadsheet()
-    df = spreadsheet_handler.df
-
-    current_datetime = datetime.datetime.now()
-
-    for index, row in df.iterrows():
-        need_to_publish_row = all(
-            [
-                current_datetime.weekday() == row['publish_day'],
-                current_datetime.time().hour == row['publish_time'],
-                not row['is_published'],
-            ]
-        )
-        if not need_to_publish_row:
-            continue
-
-        process_row_from_dataframe(
-            row,
-            spreadsheet_handler,
-            drive_handler,
-            social_platform_post_config,
-            telegram_social,
-            vk_social,
-            facebook_social,
-        )
-        break
-
-
-def process_row_from_dataframe(
-        row,
-        spreadsheet_handler,
-        drive_handler,
-        social_platform_post_config,
-        telegram_social,
-        vk_social,
-        facebook_social,
-):
-    img_file_path = drive_handler.load_file_from_google_drive_to_disk(
-        row['image_id'], img_directory
-    )
-
-    article_file_path = drive_handler.load_file_from_google_drive_to_disk(
-        row['article_id'], article_directory, 'text/plain'
-    )
-
-    with open(article_file_path, 'r') as file:
-        article_text = file.read()
-
-    if row['telegram']:
-        telegram_social.post_with_image_in_social_media(
-            text=article_text, image_path=img_file_path, **social_platform_post_config
-        )
-    if row['vk']:
-        vk_social.post_with_image_in_social_media(
-            text=article_text, image_path=img_file_path, **social_platform_post_config
-        )
-    if row['facebook']:
-        facebook_social.post_with_image_in_social_media(
-            text=article_text, image_path=img_file_path, **social_platform_post_config
-        )
-    spreadsheet_handler.mark_row_as_published(row['target_column'])
-
 
 if __name__ == '__main__':
     load_dotenv()
@@ -134,6 +21,15 @@ if __name__ == '__main__':
     spreadsheet_id = os.getenv('SPREADSHEET_ID')
     seconds_to_sleep = int(os.getenv('SECONDS_TO_SLEEP'))
 
-    while True:
-        process_spreadsheet(spreadsheet_id)
-        time.sleep(seconds_to_sleep)
+    google_scope = [
+        'https://www.googleapis.com/auth/spreadsheets',
+        'https://www.googleapis.com/auth/drive',
+    ]
+
+    service_account_credentials = os.getenv('GOOGLE_SERVICE_ACCOUNT_CREDENTIALS')
+
+    spreadsheet_service = initialize_google_spreadsheet_service(
+        google_scope, service_account_credentials
+    )
+
+    driver_service = initialize_google_drive_service(google_scope, service_account_credentials)
--- utils/spreadsheet_handler.py
+++ utils/spreadsheet_handler.py
@@ -7,6 +7,16 @@ import pandas as pd
 
 extractor = URLExtract()
 
+days_of_week = {
+    "понедельник": 0,
+    "вторник": 1,
+    "среда": 2,
+    "четверг": 3,
+    "пятница": 4,
+    "суббота": 5,
+    "воскресенье": 6,
+}
+
 
 def extract_id_from_google_drive_url(google_drive_url: str):
     res = urlparse(google_drive_url)
@@ -40,97 +50,156 @@ def extract_boolean_value(val: str):
     return None
 
 
-def extract_day_of_week(day_name: str):
-    days_dict = {
-        "понедельник": 0,
-        "вторник": 1,
-        "среда": 2,
-        "четверг": 3,
-        "пятница": 4,
-        "суббота": 5,
-        "воскресенье": 6,
-    }
-    return days_dict[day_name]
+# def extract_day_of_week(day_name: str):
+#     days_dict = {
+#         "понедельник": 0,
+#         "вторник": 1,
+#         "среда": 2,
+#         "четверг": 3,
+#         "пятница": 4,
+#         "суббота": 5,
+#         "воскресенье": 6,
+#     }
+#     return days_dict[day_name]
 
 
-class SpreadSheetHandler:
-    def __init__(self, google_spreadsheet_service: Resource, spreadsheet_id: str):
-        self.spreadsheet_service = google_spreadsheet_service
-        self.spreadsheet_id = spreadsheet_id
-        self._df = None
+def parse_spreadsheet(google_spreadsheet_service: Resource, spreadsheet_id: str):
+    sheet = google_spreadsheet_service.spreadsheets()
 
-    @property
-    def df(self) -> pd.DataFrame:
-        return self._df
+    social_platform_headers_range = 'A2:C2'
+    data_headers_range = 'D1:H1'
+    data_range = 'A3:H14'
+    first_data_row = 3
+    last_data_row = 14
 
-    def parse_spreadsheet(self):
-        sheet = self.spreadsheet_service.spreadsheets()
-
-        social_platform_headers_range = 'A2:C2'
-        data_headers_range = 'D1:H1'
-        data_range = 'A3:H14'
-        first_data_row = 3
-        last_data_row = 14
-
-        headers_social = (
-            sheet.values()
-            .get(spreadsheetId=self.spreadsheet_id, range=social_platform_headers_range)
+    headers_social = (
+        sheet.values()
+            .get(spreadsheetId=spreadsheet_id, range=social_platform_headers_range)
             .execute()
-        )
-        headers_data = (
-            sheet.values()
-            .get(spreadsheetId=self.spreadsheet_id, range=data_headers_range)
+    )
+    headers_data = (
+        sheet.values()
+            .get(spreadsheetId=spreadsheet_id, range=data_headers_range)
             .execute()
-        )
+    )
 
-        headers = [*headers_social['values'][0], *headers_data['values'][0]]
+    headers = [*headers_social['values'][0], *headers_data['values'][0]]
 
-        data = (
-            sheet.values()
+    data = (
+        sheet.values()
             .get(
-                spreadsheetId=self.spreadsheet_id,
-                range=data_range,
-                valueRenderOption='FORMULA',
-            )
+            spreadsheetId=spreadsheet_id,
+            range=data_range,
+            valueRenderOption='FORMULA',
+        )
             .execute()
-        )
-        df = pd.DataFrame(columns=headers, data=data['values'])
-        df = df.rename(
-            columns={
-                'ВКонтакте': 'vk',
-                'Телеграм': 'telegram',
-                'Фейсбук': 'facebook',
-                'День публикации': 'publish_day',
-                'Время публикации': 'publish_time',
-                'Статья': 'article_id',
-                'Картинки': 'image_id',
-                'Опубликовано?': 'is_published',
-            }
-        )
+    )
+    df = pd.DataFrame(columns=headers, data=data['values'])
+    df = df.rename(
+        columns={
+            'ВКонтакте': 'vk',
+            'Телеграм': 'telegram',
+            'Фейсбук': 'facebook',
+            'День публикации': 'publish_day',
+            'Время публикации': 'publish_time',
+            'Статья': 'article_id',
+            'Картинки': 'image_id',
+            'Опубликовано?': 'is_published',
+        }
+    )
 
-        df['vk'] = df['vk'].apply(extract_boolean_value)
-        df['telegram'] = df['telegram'].apply(extract_boolean_value)
-        df['facebook'] = df['facebook'].apply(extract_boolean_value)
-        df['publish_day'] = df['publish_day'].apply(extract_day_of_week)
-        df['publish_time'] = df['publish_time']
-        df['article_id'] = df['article_id'].apply(extract_google_drive_id_from_text)
-        df['image_id'] = df['image_id'].apply(extract_google_drive_id_from_text)
-        df['is_published'] = df['is_published'].apply(extract_boolean_value)
-        df['target_column'] = [
-            f'H{index}' for index in range(first_data_row, last_data_row + 1)
-        ]
+    df['vk'] = df['vk'].apply(extract_boolean_value)
+    df['telegram'] = df['telegram'].apply(extract_boolean_value)
+    df['facebook'] = df['facebook'].apply(extract_boolean_value)
+    df['publish_day'] = df['publish_day'].apply(lambda x: days_of_week[x])
+    df['publish_time'] = df['publish_time']
+    df['article_id'] = df['article_id'].apply(extract_google_drive_id_from_text)
+    df['image_id'] = df['image_id'].apply(extract_google_drive_id_from_text)
+    df['is_published'] = df['is_published'].apply(extract_boolean_value)
+    df['target_column'] = [
+        f'H{index}' for index in range(first_data_row, last_data_row + 1)
+    ]
 
-        self._df = df
+    return df
 
-    def mark_row_as_published(self, row_range: str):
-        sheet = self.spreadsheet_service.spreadsheets()
-        result = (
-            sheet.values()
-            .update(
-                spreadsheetId=self.spreadsheet_id,
-                range=row_range,
-                valueInputOption='RAW',
-                body={'majorDimension': 'COLUMNS', 'values': [['да']]},
-            )
-            .execute()
-        )
+# class SpreadSheetHandler:
+#     def __init__(self, google_spreadsheet_service: Resource, spreadsheet_id: str):
+#         self.spreadsheet_service = google_spreadsheet_service
+#         self.spreadsheet_id = spreadsheet_id
+#         self._df = None
+#
+#     @property
+#     def df(self) -> pd.DataFrame:
+#         return self._df
+#
+#     def parse_spreadsheet(self):
+#         sheet = self.spreadsheet_service.spreadsheets()
+#
+#         social_platform_headers_range = 'A2:C2'
+#         data_headers_range = 'D1:H1'
+#         data_range = 'A3:H14'
+#         first_data_row = 3
+#         last_data_row = 14
+#
+#         headers_social = (
+#             sheet.values()
+#             .get(spreadsheetId=self.spreadsheet_id, range=social_platform_headers_range)
+#             .execute()
+#         )
+#         headers_data = (
+#             sheet.values()
+#             .get(spreadsheetId=self.spreadsheet_id, range=data_headers_range)
+#             .execute()
+#         )
+#
+#         headers = [*headers_social['values'][0], *headers_data['values'][0]]
+#
+#         data = (
+#             sheet.values()
+#             .get(
+#                 spreadsheetId=self.spreadsheet_id,
+#                 range=data_range,
+#                 valueRenderOption='FORMULA',
+#             )
+#             .execute()
+#         )
+#         df = pd.DataFrame(columns=headers, data=data['values'])
+#         df = df.rename(
+#             columns={
+#                 'ВКонтакте': 'vk',
+#                 'Телеграм': 'telegram',
+#                 'Фейсбук': 'facebook',
+#                 'День публикации': 'publish_day',
+#                 'Время публикации': 'publish_time',
+#                 'Статья': 'article_id',
+#                 'Картинки': 'image_id',
+#                 'Опубликовано?': 'is_published',
+#             }
+#         )
+#
+#         df['vk'] = df['vk'].apply(extract_boolean_value)
+#         df['telegram'] = df['telegram'].apply(extract_boolean_value)
+#         df['facebook'] = df['facebook'].apply(extract_boolean_value)
+#         df['publish_day'] = df['publish_day'].apply(extract_day_of_week)
+#         df['publish_time'] = df['publish_time']
+#         df['article_id'] = df['article_id'].apply(extract_google_drive_id_from_text)
+#         df['image_id'] = df['image_id'].apply(extract_google_drive_id_from_text)
+#         df['is_published'] = df['is_published'].apply(extract_boolean_value)
+#         df['target_column'] = [
+#             f'H{index}' for index in range(first_data_row, last_data_row + 1)
+#         ]
+#
+#         self._df = df
+#
+#     def mark_row_as_published(self, row_range: str):
+#         sheet = self.spreadsheet_service.spreadsheets()
+#         result = (
+#             sheet.values()
+#             .update(
+#                 spreadsheetId=self.spreadsheet_id,
+#                 range=row_range,
+#                 valueInputOption='RAW',
+#                 body={'majorDimension': 'COLUMNS', 'values': [['да']]},
+#             )
+#             .execute()
+#         )
